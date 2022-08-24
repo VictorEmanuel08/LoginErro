aa39d040f2d2a39cb1794bd7fb27d0c6b16c37b3
Victor Emanuel <victorem3181@gmail.com>
2022-08-24 10:08:19 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,42 @@
-import { useContext } from 'react';
-import './App.css';
-import { Route, Routes, Link } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { Private } from './pages/Private';
-import { RequireAuth } from './contexts/Auth/RequireAuth';
-import { AuthContext } from './contexts/Auth/AuthContext';
+import { useContext } from 'react'
+import { Route, Routes, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import { RequireAuth } from './contexts/Auth/RequireAuth'
+import { AuthContext } from './contexts/Auth/AuthContext'
+import Login from './pages/Login'
 
 function App() {
-  const auth = useContext(AuthContext);
+  const auth = useContext(AuthContext)
 
   const handleLogout = async () => {
-    await auth.signout();
-    window.location.href = window.location.href;
+    await auth.signout()
+    window.location.href = window.location.href
   }
 
   return (
-    <div className="App">
-      <header>
-        <h1>Header do site</h1>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/private">Página Privada</Link>
-          {auth.user && <button onClick={handleLogout}>Sair</button>}
-        </nav>
-      </header>
-      <hr />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/private" element={<RequireAuth><Private /></RequireAuth>} />
-      </Routes>
-    </div>
-  );
+    // <div className="App">
+    //   <header>
+    //     <h1>Header do site</h1>
+    //     <nav>
+    //       <Link to="/">Home</Link>
+    //       <Link to="/private">Página Privada</Link>
+    //       {auth.user && <button onClick={handleLogout}>Sair</button>}
+    //     </nav>
+    //   </header>
+    //   <hr />
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route
+        path="/home"
+        element={
+          <RequireAuth>
+            <Home />
+          </RequireAuth>
+        }
+      />
+    </Routes>
+    // </div>
+  )
 }
 
-export default App;
+export default App
--- src/contexts/Auth/RequireAuth.tsx
+++ src/contexts/Auth/RequireAuth.tsx
@@ -1,13 +1,13 @@
-import { useContext } from "react";
-import { Login } from "../../pages/Login";
-import { AuthContext } from "./AuthContext";
+import { useContext } from 'react'
+import Login from '../../pages/Login'
+import { AuthContext } from './AuthContext'
 
 export const RequireAuth = ({ children }: { children: JSX.Element }) => {
-    const auth = useContext(AuthContext);
+  const auth = useContext(AuthContext)
 
-    if (!auth.user) {
-        return <Login />;
-    }
+  if (!auth.user) {
+    return <Login />
+  }
 
-    return children;
+  return children
 }
